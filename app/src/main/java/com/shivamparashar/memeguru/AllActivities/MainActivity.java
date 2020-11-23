b1975179473b8bbcea2241ce7ca7daa3bcbde42f
Shivam Jha <shivamparashar165@gmail.com>
2020-11-23 16:00:09 +0530

    Splash Screen.

--- app/src/main/java/com/shivamparashar/memeguru/AllActivities/MainActivity.java
+++ app/src/main/java/com/shivamparashar/memeguru/AllActivities/MainActivity.java
@@ -1,8 +1,9 @@
-package com.shivamparashar.memeguru;
+package com.shivamparashar.memeguru.AllActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/shivamparashar/memeguru/AllActivities/SplashActivity.java
+++ app/src/main/java/com/shivamparashar/memeguru/AllActivities/SplashActivity.java
@@ -1,16 +1,42 @@
 package com.shivamparashar.memeguru.AllActivities;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.shivamparashar.memeguru.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }

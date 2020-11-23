e9113ade4cd98f6a868e44f6170b66244ad7390c
yashbhardwaj <yashsharma123rrr@gmail.com>
2020-11-23 15:25:10 +0530

    1st

---
+++ app/src/main/java/com/shivamparashar/memeguru/AllActivities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.shivamparashar.memeguru.AllActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shivamparashar.memeguru.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
